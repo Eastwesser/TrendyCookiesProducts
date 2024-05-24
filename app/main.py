bd0964ae2abae8f318190b3ff458a3131d82ce5d
Dennis Mattews <alttert@mail.ru>
2024-05-25 01:30:05 +0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from .database import Base, SessionLocal
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_in_transaction(Base.metadata.create_all)
