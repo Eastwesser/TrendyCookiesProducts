36fa89a087a2e635ec24a589c060d12ebc051e47
Dennis Mattews <alttert@mail.ru>
2024-05-27 03:14:18 +0300

    CookieMiu Backend #10

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from app.data import products_data
+from orders.orders_db import save_user_info
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -16,6 +17,17 @@ async def get_products(request: Request):
             "products_data": products_data
         }
     )
+
+
+@app.post("/order")
+async def create_order(request: Request, product_id: int = Form(...), name: str = Form(...), phone: str = Form(...)):
+    order_id = save_user_info(product_id, name, phone)
+    return {"message": f"Order for product with ID {product_id} created with order ID {order_id}"}
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == "__main__":
